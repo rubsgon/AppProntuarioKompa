d5254d48cc8bae5216e10006389ef654e0f26413
Rubens Gonçalves <rubensgca@gmail.com>
2021-09-20 18:06:01 -0300

    adicionado mais cores

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -2,7 +2,11 @@ const colors = {
   background: '#F2F2F2',
   buttonPrimary: '#D9115A',
   textPrimary: '#262626',
-  textWhite: '#F9EDF1',
+  textSecundary: '#F9EDF1',
+  green: '#49A69C',
+  blackgreen: '#006464',
+  lightgreen: '#B0D9D5',
+  grey: '#C6C6C6',
 }
 
 export default colors
